835bf38b450a5fa043605399d290bd991205083f
thinkpuppy <341181484@gapps.yrdsb.ca>
2021-04-13 19:10:34 -0400

    add setkey support to commands using api keys

--- commands/matches.js
+++ commands/matches.js
@@ -1,17 +1,26 @@
 const axios = require('axios');
+const tokenData = require('../schemas/tokenschema');
 
 module.exports = {
     name: "matches",
     description: "Accepts your bwtinder matches",
-    execute(client, message, args, Discord) {
-        message.delete()
-        let apiKey = args[0];
+    async execute(client, message, args, Discord) {
+        let apiKey;
+        let user = await tokenData.findOne({ userID: message.author.id});
+
+        if (args[0]) {
+            message.delete();
+            apiKey = args[0];
+        } else if (user) apiKey = user.apiToken;
+
         if (!apiKey) {
             message.channel.send(`Error: No API Key Specified. To get this, go to: https://bit.ly/3t8vTc9`)
         } else {
             axios.post('https://bwtinder.com/api/profile', {
                 token: apiKey
             }).then((res) => {
+                if (!res.data.success) return message.reply('`The api request failed, this may be because of an incorrect api token.`');
+
                 let matches = res.data.matches;
                 if (matches.length === 0) {
                     const noMatchesEmbed = new Discord.MessageEmbed()
--- commands/profile.js
+++ commands/profile.js
@@ -1,17 +1,26 @@
 const axios = require('axios');
+const tokenData = require('../schemas/tokenschema');
 
 module.exports = {
     name: "profile",
     description: "Checks your bwtinder profile",
-    execute(client, message, args, Discord) {
-        let apiKey = args[0];
+    async execute(client, message, args, Discord) {
+        let apiKey;
+        let user = await tokenData.findOne({ userID: message.author.id});
+
+        if (args[0]) {
+            message.delete();
+            apiKey = args[0];
+        } else if (user) apiKey = user.apiToken;
+
         if (!apiKey) {
             message.channel.send(`Error: No API Key Specified. To get this, go to: https://bit.ly/3t8vTc9`)
         } else {
-            message.delete();
             axios.post('https://bwtinder.com/api/user', {
                 token: apiKey
             }).then((res) => {
+                if (!res.data.success) return message.reply('`The api request failed, this may be because of an incorrect api token.`');
+
                 const embed = new Discord.MessageEmbed()
                     .setTitle(`${message.author.tag}'s BWTinder Profile`)
                     .setDescription(`Bio: ${res.data.bio}`)
