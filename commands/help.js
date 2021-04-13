f12de8edf6c4bb7c7f38fbc03b85258fe14f314d
CephalonOrdis <ceqhalon@gmail.com>
2021-04-13 16:01:34 +0100

    Added a field that explains the below fields and reworked the names of the command fields

--- commands/help.js
+++ commands/help.js
@@ -9,9 +9,10 @@ module.exports = {
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
                 { name: "Prefix", value: "The prefix for the bot is `\*\`"},
-                { name: "matches", value: "Shows how many people you have available. If you have any, you will auto accept them. It swipes for you!"},
-                { name: "profile", value: "Displays your profile on bedwars tinder. It displays your age, discord id, uuid, ign, date of birth, etc."},
-                { name: "setkey", value: "Placeholder Value"}
+                { name: "Commands", value: "Below is a list of commands that may be executed by typing in chat the prefix followed by command name:\n---------------------------------------------------------------------"},
+                { name: "• matches", value: "Shows how many people you have available. If you have any, you will auto accept them. It swipes for you!"},
+                { name: "• profile", value: "Displays your profile on bedwars tinder. It displays your age, discord id, uuid, ign, date of birth, etc."},
+                { name: "• setkey", value: "Placeholder Value"}
             )
             .setFooter("As of now, you can check your profile only with api key. Contact BizarreAvatar#8346 if anything bugs out", message.author.displayAvatarURL())
             .setTimestamp()
