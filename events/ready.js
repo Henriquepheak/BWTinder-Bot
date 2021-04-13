8e8098f4b1cd145a93cb202605075cfa1d1369ac
andrewtam6 <55567726+andrewtam6@users.noreply.github.com>
2021-04-12 21:22:21 -0500

    Testing MongoDB support

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,22 @@
+const mongo = require('../utils/mongoose')
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute (client) {
         console.log(`Logged in as ${client.user.tag}`)
 
         client.user.setPresence({
             activity: { name: `BWTinder Bot is helping people find love in ${client.guilds.cache.size} servers! | *help`},
             status: "dnd"
+        })
+
+        await mongo().then((mongoose) => {
+            try {
+                console.log('MongoDB Connected Successfully!')
+            } finally {
+                mongoose.connection.close()
+            }
         })
     }
 }
--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,28 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
-const connectionURL = 'mongodb+srv://admin:JeffyWhy1753215@cluster0.bzgs3.mongodb.net/Tokens?retryWrites=true&w=majority?authSource=admin'
+const connectionURL = process.env.MONGODB_PATH
 
-module.exports = {
-    init: () => {
-        const dbOptions = {
-            useUnifiedTopology: true,
-            autoIndex: false,
-            poolSize: 5,
-            connectTimeoutMS: 10000,
-        };
-
-        mongoose.connect(connectionURL, dbOptions);
-        mongoose.set('useFindAndModify', false);
-        mongoose.Promise = global.Promise;
-        mongoose.connection.on('connected', () => {
-            console.log('Mongoose has been connected successfully!');
-        });
-
-        mongoose.connection.on('err', err => {
-            console.error(`Mongoose connection error`);
-        });
-
-        mongoose.connection.on('disconnected', () => {
-            console.warn('Mongoose connection lost');
-        });
-    }
+module.exports = async () => {
+    await mongoose.connect(connectionURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }) 
+    return mongoose
 }
