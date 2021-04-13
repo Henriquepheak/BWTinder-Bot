b517118bcaecefe9de509b1d351e45fc93f9070a
andrewtam6 <55567726+andrewtam6@users.noreply.github.com>
2021-04-12 21:16:09 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
             .setDescription("Need help? Look no further!")
             .setThumbnail(client.user.displayAvatarURL())
             .addFields(
-                { name: "Prefix", value: "The prefix for the bot is \"*\""},
+                { name: "Prefix", value: "The prefix for the bot is `\"*\"`"},
                 { name: "matches", value: "Shows how many people you have available. If you have any, you will auto accept them. It swipes for you!"},
                 { name: "profile", value: "Displays your profile on bedwars tinder. It displays your age, discord id, uuid, ign, date of birth, etc."},
                 { name: "setkey", value: "Placeholder Value"}
@@ -16,7 +16,5 @@ module.exports = {
             .setFooter("As of now, you can check your profile only with api key. Contact BizarreAvatar#8346 if anything bugs out", message.author.displayAvatarURL())
             .setTimestamp()
         message.channel.send(embed)
-        
-        )
     }
 }
