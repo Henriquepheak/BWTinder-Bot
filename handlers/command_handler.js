abd4548f6d94db5cd5aa11793255186891002e7f
CephalonOrdis <ceqhalon@gmail.com>
2021-04-15 15:05:15 +0100

    Upgraded the command handler to run files within a folder inside of the commands folder

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 module.exports = (client, Discord) => {
-    const command_files = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+    const command_folders = fs.readdirSync('./commands/');
 
-    for(const file of command_files) {
-        const command = require(`../commands/${file}`);
+    for(const folder of command_folders) {
+        const command_files = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+        for (const file of command_files) {
+            const command = require(`../commands/${folder}/${file}`);
 
-        if (command.name) {
-            client.commands.set(command.name, command)
-        } else {
-            continue;
+            if (command.name) {
+                client.commands.set(command.name, command)
+            } else {
+                continue;
+            }
         }
     }
 }
